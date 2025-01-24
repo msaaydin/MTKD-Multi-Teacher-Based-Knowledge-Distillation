9941f1b34d48bd664d9e32bdc3089d06c1d01265
Musa AYDIN <aydn.msa@gmail.com>
2025-01-24 15:26:24 +0300

    updated_DCA1

---
+++ KD_DCA1/data.py
@@ -0,0 +1,34 @@
+import os
+from PIL import Image
+import torch
+import torchvision.transforms.functional as TF
+from torch.utils.data import Dataset
+from torchvision import transforms
+import numpy as np
+from glob import glob
+class MultiOutputImageDataset(Dataset):
+    def __init__(self, input_dir, target_dir, transform=None):
+        self.input_dir = input_dir
+        self.transform = transform
+
+        # Get all input images
+        self.input_images = glob( input_dir + "/*.png", recursive=True)
+        self.target_path = target_dir
+
+    def __len__(self):
+        return len(self.input_images)
+
+    def __getitem__(self, idx):
+        # Load input image
+        target_image_path = os.path.join(self.target_path,os.path.basename(self.input_images[idx]))
+        input_image_path =  self.input_images[idx]
+        input_image = Image.open(input_image_path).convert('L')
+        out_im = Image.open(target_image_path).convert("L")  # Convert to grayscale (1 channel)
+        input_image = TF.pad(input_image, (0, 0, 20, 20))
+        out_im = TF.pad(out_im, (0, 0, 20, 20))
+        # Apply transforms if provided
+        if self.transform:
+            input_image = self.transform(input_image)
+            out_im = self.transform(out_im)
+        
+        return input_image, out_im
